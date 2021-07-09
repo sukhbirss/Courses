9088a322b7fab4a6ab96abab56a4801962b9fc24
sukhbirss <s.sukhbirsohal@gmail.com>
2021-07-09 08:53:28 +0530

    animations

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,12 @@
 import styles from "./card.module.css";
-export default function Card({image,title}){
+export default function Card({image,title,author,rating,price}){
   return (
     <div className={styles.card}>
       <img src={image} />
       <p>{title}</p>
+      <p>{author}</p>
+      <p>{rating}</p>
+      <p>{price}</p>
     </div>
   )
 }
--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -2,7 +2,7 @@ import styles from "./courses.module.css";
 import Card from '../card/Card'
 
 const DATA = [	
-				{image:"https://img-c.udemycdn.com/course/240x135/1362070_b9a1_2.jpg",title:"React - The Complete Guide (incl Hooks, React Router, Redux)",author:"sukhbir",rating:"",price:""},
+				{image:"https://img-c.udemycdn.com/course/240x135/1362070_b9a1_2.jpg",title:"React - The Complete Guide (incl Hooks, React Router, Redux)",author:"sukhbir",rating:"4.5",price:"499"},
 				{image:"https://img-c.udemycdn.com/course/240x135/2121018_9de5_5.jpg",title:"Redux",author:"",rating:"",price:""},
 				{image:"https://img-c.udemycdn.com/course/240x135/2121018_9de5_5.jpg",title:"React - The Complete Guide (incl Hooks, React Router, Redux",author:"sukhbir",rating:"",price:""},
 				{image:"https://img-c.udemycdn.com/course/240x135/705264_caa9_11.jpg",title:"Redux",author:"sukhbir",rating:"",price:""},
@@ -14,11 +14,13 @@ const DATA = [
 export default function Home() {
   return (
     <div className={styles.wrapper}>
+    	<h1>lets find a course for you</h1>
+
     	<div className={styles.container}>
     	{
     		DATA.map((el)=>{
     			return (
-    				<Card image={el.image} title={el.title}/>
+    				<Card image={el.image} title={el.title} author={el.author} rating={el.rating} price={el.price}/>
     				)
     		})
     	}
