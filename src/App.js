025f3de3a973090e99974457c0a19f76937385b7
sukhbirss <s.sukhbirsohal@gmail.com>
2021-07-09 20:18:48 +0530

    its coming to an end

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import './App.css';
 import Home from './page/Home'
 import { Route,Switch,useHistory } from 'react-router-dom'
 import Detail from './page/detail/Detail'
+import Welcome from './page/welcome/Welcome'
 
-const Routing = ()=>{
-  return(
-        <Switch>
-          <Route exact path="/:id" component={Home}/>
-          <Route exact path="/detail/:id" component={Detail}/>
-      	</Switch>
-  )
-}
 
 function App() {
   return (
-        <Routing />
+        <Switch>
+          <Route exact path="/" component={Welcome}/>
+          <Route exact path="/:id" component={Home}/>
+          <Route exact path="/detail/:id" component={Detail}/>
+      	</Switch>
   );
 }
 
